2cff1fa7df4d0cabecf100b0c63bd1741cd1fab0
mandelshtamd <mandelstudy@gmail.com>
2020-03-29 02:04:18 +0300

    added mutation of arithmetic expression, if-statement and variable declaration

---
+++ src/main/kotlin/com/stepanov/bbf/bugfinder/deadCodeMutator/equivalenceTransformations/AddVariableDeclaration.kt
@@ -0,0 +1,15 @@
+package com.stepanov.bbf.bugfinder.deadCodeMutator.equivalenceTransformations
+
+import com.stepanov.bbf.bugfinder.mutator.transformations.Transformation
+import org.jetbrains.kotlin.psi.*
+
+import com.stepanov.bbf.bugfinder.util.getAllPSIChildrenOfType
+import com.stepanov.bbf.bugfinder.util.getRandomVariableName
+import java.util.*
+
+class AddVariableDeclaration : Transformation() {
+    override fun transform() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
