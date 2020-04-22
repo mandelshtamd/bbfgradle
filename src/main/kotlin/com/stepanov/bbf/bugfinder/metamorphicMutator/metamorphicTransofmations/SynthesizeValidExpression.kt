f65ab998efd91b1538b97cd945364345a8d9b5a6
mandelshtamd <mandelstudy@gmail.com>
2020-04-22 09:39:02 +0300

    added synthesizeValidExpression class

---
+++ src/main/kotlin/com/stepanov/bbf/bugfinder/metamorphicMutator/metamorphicTransofmations/SynthesizeValidExpression.kt
@@ -0,0 +1,50 @@
+package com.stepanov.bbf.bugfinder.metamorphicMutator.metamorphicTransofmations
+
+import kotlin.random.Random
+
+class SynthesizeValidExpression {
+    fun synExpression(env: MutableMap<String, List<Int>>): String {
+        val worklist = toSample(env.keys, Random.nextInt(env.keys.size))
+        var unaryMaximumCount = 5
+
+        while (worklist.size > 1) {
+            if (Random.nextBoolean() && unaryMaximumCount-- > 0) { // unary expression
+                val v = toSample(worklist, 1)
+                val unaryOperationList = unaryOperators.shuffled()
+                for (uop in unaryOperationList) {
+                    worklist.removeAll(v)
+                    worklist.add(composeExpression(uop, "", v.first()))
+                    //TODO:add correctness checking
+                    break
+                }
+
+            } else { // binary expression
+                val operands = toSample(worklist, 2)
+                val binaryOperationList = binaryOperators.shuffled()
+                for (bop in binaryOperationList) {
+                    worklist.removeAll(operands)
+                    worklist.add(composeExpression(bop, operands.elementAt(0), operands.elementAt(1)))
+                    //TODO:add correctness checking
+                    break
+                }
+            }
+        }
+        return worklist.first()
+    }
+
+    fun toSample(env: MutableSet<String>, untilNum: Int) : MutableSet<String> {
+        val sample = mutableSetOf(env.elementAt(0).substringAfter('.'))
+        for (num in 1 until untilNum)
+            sample.add(env.elementAt(num).substringAfter('.'))
+        return sample
+    }
+
+    fun composeExpression(operation : String, left_pred : String, right_pred : String) : String{
+        if (left_pred == "")
+            return "($operation$right_pred)"
+        return "($left_pred) $operation ($right_pred)"
+    }
+
+    val unaryOperators = listOf("!", "~", "-")
+    val binaryOperators = listOf("+", "-", "*", "/", "%", "<<", ">>", "&", "|", "^", ">", ">=", "==", "!=", "<=", "<")
+}
